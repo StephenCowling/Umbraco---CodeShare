025eec5897dd4c94f9113432e64127d389187e4d
Steve Cowling <stephen.cowling@kiwiplates.nz>
2017-07-07 11:24:38 +1200

    Add project files.

---
+++ EngineRoomInk/Controllers/SiteLayoutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+namespace EngineRoomInk.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        public ActionResult RenderHeader()
+        {
+            return PartialView("~/Views/Partials/SiteLayout/Header.cshtml");
+        }
+
+    }
+}
